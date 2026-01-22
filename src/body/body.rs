9ab4a0d7109bea4c6176337927a02d1b3d6fe17a
Thiago S. Marcelino <thiagodsmarcelino@gmail.com>
2026-01-22 10:15:27 -0300

    feat: implement test cases for body parse

--- src/body/body.rs
+++ src/body/body.rs
@@ -16,3 +16,40 @@ pub(crate) fn parse(
 
     Ok((done, read))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_standard_body() {
+        let mut body: Vec<u8> = Vec::new();
+        let data = b"Hello, World!";
+        let content_length = data.len();
+
+        let result = parse(&mut body, data, content_length);
+        assert!(result.is_ok());
+
+        let (done, consumed) = result.unwrap();
+        assert!(done);
+        assert_eq!(consumed, content_length);
+
+        assert_eq!(body, data);
+    }
+
+    #[test]
+    fn test_body_shorter_than_reported_content_length() {
+        let mut body: Vec<u8> = Vec::new();
+        let data = b"Hello, World!";
+        let content_length = data.len() * 2;
+
+        let result = parse(&mut body, data, content_length);
+        assert!(result.is_ok());
+
+        let (done, consumed) = result.unwrap();
+        assert!(!done);
+        assert_eq!(consumed, data.len());
+
+        assert_eq!(body, data);
+    }
+}
